3dc7e01aadf9006eb2b10b9da8d63782bb7ae226
nicholasbrar <153023837+nicholasbrar@users.noreply.github.com>
2024-09-13 13:43:01 -0400

    chagnes

--- Chess Engine/main.cpp
+++ Chess Engine/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -24,8 +25,8 @@ public:
 
 	void initializeBoard() {
 		for (int i = 0; i < SIZE; i++) {	 // initialize pawns
-			board[1][i] = { PAWN, WHITE };
 			board[6][i] = { PAWN, BLACK };
+			board[1][i] = { PAWN, WHITE };
 		}
 
 		board[0][0] = board[0][7] = { ROOK, WHITE }; // initialize rooks
@@ -87,8 +88,8 @@ public:
 	}	   
 
 	bool isValidMove(int startX, int startY, int endX, int endY) {
-		SQUARE start = board[startX][startX];
-		SQUARE end = board[endX][endX];
+		SQUARE start = board[startX][startY];
+		SQUARE end = board[endX][endY];
 
 		if (start.piece == NONE) { // can't move a piece with no square
 			return false;
@@ -117,30 +118,26 @@ public:
 	}
 
 	bool isValidPawnMove(int startX, int startY, int endX, int endY, COLOR color) {
-		int direction; 
+		int direction = (color == WHITE) ? 1 : -1; // push up/down board
 
-		if (color == WHITE) {	// white pawn push or black pawn push
-			direction = 1;
-		}
-		else {
-			direction = -1;
-		}
+		int pawnStart = (color == WHITE) ? 1 : 6; // to check for first move (2 squares)
 
 		if (startY == endY && board[endX][endY].piece == NONE) { // vertical pawn push
-			if (endX = startX + direction) {
+			if (endX == startX + direction) {
 				return true;
 			}
-		}
 
-		if ((startX == 1 && color == WHITE || startX == 6 && color == BLACK) && endX == startX + (2 * direction)) {
-			if (board[endX][endY].piece == NONE) {
-				return true;
+			if (startX == pawnStart && endX == startX + 2 * direction) {
+				if (board[endX][endY].piece == NONE && board[startX + direction][startY].piece == NONE) {
+					return true;
+				}
 			}
 		}
 
 		if (abs(startY - endY) == 1 && endX == startX + direction && board[endX][endY].piece != NONE) {
 			return true;
 		}
+		return false;
 
 	}
 
@@ -165,17 +162,36 @@ public:
 	}
 
 	void getMove(int& startX, int& startY, int& endX, int& endY) {
-		cout << "Enter move (e2 e4) ";
+		cout << endl << endl << "Enter move(e2 e4d) ";
 		string move;
-		cin >> move; 
 
-		//string str = parseMove(move);
+		cout << " TEST";
+		
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		getline(cin, move);
+		cout << " move entered   " << move << endl;
+		cout.flush();
+
+		if (move.length() != 5 || move[2] != ' ') {
+			cout << "Invalid move format  !! ";
+			return;
+		}
+																				 
+		startY = move[0] - 'a'; 
+		startX = SIZE - (move[1] - '0') ; 
+		endY = move[3] - 'a';   
+		endX = SIZE - (move[4] - '0') ;
+		std::cout << startY << " " << startX << " " << endY << " " << endX << endl;
+		
+
 	}
+
 
 	void playGame() {
 		int startX, startY, endX, endY;
 
 		while (true) {
+			cout << endl;
 			printBoard(); 
 			if (turn == WHITE) {
 				cout << "White's ";
@@ -195,9 +211,6 @@ public:
 					turn = WHITE;
 				}
 			}
-			else {
-				cout << "Invalid move";
-			}
 		}
 	}
 
@@ -207,5 +220,5 @@ public:
 
 int main() {
 	ChessBoard board;
-	board.printBoard();
+	board.playGame();
 }
